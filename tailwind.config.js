7852fd2e93b2ef8ad2239f9b10195afd94da2b27
Filip Tomes <filiptomes@protonmail.com>
2023-10-16 09:43:13 +0200

    project init setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        bdOrange: '#e96e25',
+      },
+    },
+  },
+  plugins: [],
+};
